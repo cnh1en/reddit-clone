f539473d065ccf449400e123c85455f35539f62f
cnh1en <cnhien12@gmail.com>
2023-02-20 00:10:31 +0700

    Initial app

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type NavbarProps = {};
+
+const Navbar = (props: NavbarProps) => {
+	return <div>This is Navbar</div>;
+};
+
+export default Navbar;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+	return <div>Hello</div>;
+};
+
+export default Home;
