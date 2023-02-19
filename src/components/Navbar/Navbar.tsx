d247f19da9253d2aab68df6b0b3bcb65c1db87ae
cnh1en <cnhien12@gmail.com>
2023-02-20 01:10:34 +0700

    feat: logo app

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,22 @@
+import { Box, Flex } from '@chakra-ui/react';
+import Image from 'next/image';
 import React from 'react';
 
 type NavbarProps = {};
 
-const Navbar = (props: NavbarProps) => {
-	return <div>This is Navbar</div>;
+const Navbar = () => {
+	return (
+		<Flex bg="white" height="44px" padding="6px 12px">
+			<Flex align="center">
+				<Image
+					src="/images/redditlogo.png"
+					alt="reddit"
+					width={80}
+					height={80}
+				/>
+			</Flex>
+		</Flex>
+	);
 };
 
 export default Navbar;
